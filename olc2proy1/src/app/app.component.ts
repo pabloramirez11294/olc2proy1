2741085ee2541099d9b6f74ee65580dddfd7b4a8
pabloramirez11294 <pabloramirez.11294@gmail.com>
2020-08-28 16:39:20 -0600

    Agrego code mirror.

--- olc2proy1/src/app/app.component.ts
+++ olc2proy1/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+//import Traduccion from './gramaticas/traduccion';
+//import Ejecucion from './gramaticas/ejecucion';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'olc2proy1';
+  editor = "console.log(5+99+8+7+5);";
+  consola = "";
+  ejecutar(){
+    try {
+     /* const value = Ejecucion.parse(this.editor);
+      for(const instr of value){
+        try {
+            console.log(instr);
+            const actual = instr.execute();
+            if(actual != null || actual != undefined){
+               // errores.push(new Error_(actual.line, actual.column, 'Semantico', actual.type + ' fuera de un ciclo'));
+            }
+        } catch (error) {
+            //errores.push(error);  
+            console.log(error);
+        }
+    }*/
+    } catch (error) {
+      console.log(error);
+      //alert("Aun no valido errores")
+    }
+  }
 }
--- olc2proy1/src/app/app.module.ts
+++ olc2proy1/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { CodemirrorModule } from '@ctrl/ngx-codemirror';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    CodemirrorModule,
     AppRoutingModule
   ],
   providers: [],
